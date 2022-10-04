a741120be225ecb54dbe53774e2e57f63ae64a98
IlliaButenko <95761247+IlliaButenko@users.noreply.github.com>
2022-10-04 10:42:05 -0300

    add get NFT data function
    Co-Authored-By: SuperStar <105542355+superstar323@users.noreply.github.com>

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var express = require('express');
 var app = express();
 var cors = require('cors');
 const path = require('path');
+const axios = require('axios');
+
 
 var bodyParser = require('body-parser');
 
@@ -10,21 +12,17 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors());
 // When you have your own Client ID and secret, put down their values here:
-app.use('/getNFTs', (req, res) => {
+app.use('/getNFTs', async (req, res) => {
     const { accountId, NFTContractAddress } = req.body;
-    fetch(
+    const data = await axios.get(
         `https://api-v2-mainnet.paras.id/token?owner_id=${accountId}&contract_id=${NFTContractAddress}&__limit=2000`,
-        {
-            method: "GET",
-            mode: "cors",
-        }
-    ).then((response) => response.json())
-        .then((data) => {
-            return res.json({ status: true, data: data.data.results });
-        })
-        .catch((err) => {
-            return res.json({ status: true, data: err });
-        });
+    )
+    console.log(data.data.data)
+    if (data) {
+        return res.json({ status: true, data: data.data.data })
+    } else {
+        return res.json({ status: false, data: 'err' });
+    }
 })
 
 app.use(express.static(path.join(__dirname, '/src')));
